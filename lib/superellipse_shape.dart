0f1d0dd6ef1a9bec6c1f5afd693322443cd89cf6
salby <srdlarsen@gmail.com>
2019-01-02 19:21:20 +0100

    Added untracked files to commit

---
+++ lib/superellipse_shape.dart
@@ -0,0 +1,71 @@
+import 'dart:math';
+import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+
+class SuperEllipse extends ShapeBorder {
+
+  final double n;
+  final BorderSide side;
+
+  SuperEllipse({
+    @required this.n,
+    this.side = BorderSide.none,
+  }) : assert(side != null);
+
+  @override
+  EdgeInsetsGeometry get dimensions => EdgeInsets.all(side.width);
+
+  @override
+  ShapeBorder scale(double t) {
+    return SuperEllipse(
+      n: n,
+      side: side.scale(t),
+    );
+  }
+
+  @override
+    Path getInnerPath(Rect rect, {TextDirection textDirection}) {
+    return _superEllipsePath(rect.deflate(side.width), n);
+  }
+
+  @override
+    Path getOuterPath(Rect rect, {TextDirection textDirection}) {
+    return _superEllipsePath(rect, n);
+  }
+
+  static Path _superEllipsePath(Rect rect, double n) {
+
+    Path path = new Path();
+
+    // Define steps.
+    double a = rect.width / 2;
+    double b = rect.height / 2;
+    double na = 2 / n;
+    int step = 32 * 4;
+    double piece = (pi * 2) / step;
+    double t = 0.0;
+    var apply = path.moveTo;
+
+    // Build path.
+    for (int i = 0; i < step; i++) {
+      var x = pow(cos(t).abs(), na) * a * cos(t).sign;
+      var y = pow(sin(t).abs(), na) * b * sin(t).sign;
+      apply(x, y);
+      apply = path.lineTo;
+      t+= piece;
+    }
+
+    path.close();
+
+    return path.shift(rect.center);
+  
+  }
+  
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection textDirection}) {
+    Path path = getOuterPath(rect, textDirection: textDirection);
+    var paint = side.toPaint();
+    canvas.drawPath(path, paint);
+  }
+
+}
---
+++ test/superellipse_shape_test.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutterellipse_shape/superellipse_shape.dart';
