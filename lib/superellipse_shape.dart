9fb846de6705f24d27e26c0a690813950f4d35a4
salby <srdlarsen@gmail.com>
2019-01-03 16:42:01 +0100

    Fixed a few things

--- lib/superellipse_shape.dart
+++ lib/superellipse_shape.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-import 'package:meta/meta.dart';
 import 'package:flutter/material.dart';
 
 class SuperEllipseShape extends ShapeBorder {
--- test/superellipse_shape_test.dart
+++ test/superellipse_shape_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutterellipse_shape/superellipse_shape.dart';
+import 'package:superellipse_shape/superellipse_shape.dart';
 
 void main() {
 
