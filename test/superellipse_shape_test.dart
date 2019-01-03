f9aa20d0294e51591b1af23bc76cdda570a55bba
salby <srdlarsen@gmail.com>
2019-01-03 16:36:04 +0100

    Added a test just because. I doesn't really help since it can't ever fail unless I do something really stupid.

--- test/superellipse_shape_test.dart
+++ test/superellipse_shape_test.dart
@@ -1,3 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutterellipse_shape/superellipse_shape.dart';
+
+void main() {
+
+  test('Shape test', () {
+
+    var n = 5.0;
+
+    SuperEllipseShape testShape = SuperEllipseShape(n);
+
+    expect(testShape.n, 5.0);
+
+  });
+
+}
