5155ed895ebb0135e65f27550f4475a574c7e554
salby <srdlarsen@gmail.com>
2019-01-07 16:48:06 +0100

    Changed the method of calculating the superellipse to look more even on shapes that aren't quadratic

--- lib/superellipse_shape.dart
+++ lib/superellipse_shape.dart
@@ -6,13 +6,12 @@ import 'package:flutter/material.dart';
 class SuperellipseShape extends ShapeBorder {
   /// The arguments must not be null.
   SuperellipseShape(
-    this.n, {
+    this.superRadius, {
     this.side = BorderSide.none,
-  })  : assert(n != null),
+  })  : assert(superRadius != null),
         assert(side != null);
 
-  /// n.
-  final double n;
+  final double superRadius;
 
   /// The style of this border.
   final BorderSide side;
@@ -23,47 +22,55 @@ class SuperellipseShape extends ShapeBorder {
   @override
   ShapeBorder scale(double t) {
     return SuperellipseShape(
-      n,
+      superRadius,
       side: side.scale(t),
     );
   }
 
-  Path _getPath(Rect rect, double n) {
-    /// Create empty path to complete.
-    Path path = new Path();
+  Path _getPath(Rect rect, double superRadius) {
+    /// Define the different variables of [rect].
+    final center = rect.center;
+    final double left = rect.left;
+    final double right = rect.right;
+    final double top = rect.top;
+    final double bottom = rect.bottom;
 
-    /// Define width and height, based on [rect] dimensions.
-    double width = rect.width / 2;
-    double height = rect.height / 2;
+    /// [centerX] and [centerY] are only used if [superRadius] is null.
+    final double centerX = center.dx;
+    final double centerY = center.dy;
 
-    double na = 2 / n;
-    int step = 32 * 4;
-    double piece = (math.pi * 2) / step;
-    double t = 0.0;
-    var apply = path.moveTo;
-
-    /// Build path.
-    for (int i = 0; i < step; i++) {
-      var x = math.pow(math.cos(t).abs(), na) * width * math.cos(t).sign;
-      var y = math.pow(math.sin(t).abs(), na) * height * math.sin(t).sign;
-      apply(x, y);
-      apply = path.lineTo;
-      t += piece;
+    /// Defaults to predefined shape if [superRadius] is null.
+    if (superRadius == null) {
+      return Path()
+        ..moveTo(left, centerY)
+        ..cubicTo(left, top, left, top, centerX, top)
+        ..cubicTo(right, top, right, top, right, centerY)
+        ..cubicTo(right, bottom, right, bottom, centerX, bottom)
+        ..cubicTo(left, bottom, left, bottom, left, centerY)
+        ..close();
     }
-    path.close();
 
-    /// Shift path to center of [rect].
-    return path.shift(rect.center);
+    /// Construct path from [rect] variables and [superRadius].
+    return Path()
+      ..moveTo(left, top + superRadius)
+      ..cubicTo(left, top, left, top, left + superRadius, top)
+      ..lineTo(right - superRadius, top)
+      ..cubicTo(right, top, right, top, right, top + superRadius)
+      ..lineTo(right, bottom - superRadius)
+      ..cubicTo(right, bottom, right, bottom, right - superRadius, bottom)
+      ..lineTo(left + superRadius, bottom)
+      ..cubicTo(left, bottom, left, bottom, left, bottom - superRadius)
+      ..close();
   }
 
   @override
   Path getInnerPath(Rect rect, {TextDirection textDirection}) {
-    return _getPath(rect, n);
+    return _getPath(rect, superRadius);
   }
 
   @override
   Path getOuterPath(Rect rect, {TextDirection textDirection}) {
-    return _getPath(rect, n);
+    return _getPath(rect, superRadius);
   }
 
   @override
